69298b9381f053eb314320c3d7da059dcc30a252
bofosu01 <bofosu@luc.edu>
2026-03-28 14:11:30 -0500

    Update usage instructions for extractLD_bfile.sh

--- functions/extractLD_bfile.sh
+++ functions/extractLD_bfile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Calculates the linkage disequilibrium(LD) between a top snp and the variants within a specific window using PLINK
-# Usage: bash extractLD_bfile.sh plink_path bfile_prefix keep_file chr pos_start pos_end top_snp out_file
+# Usage: bash extractLD_bfile.sh plink_path bfile_prefix chr pos_start pos_end top_snp window_kb out_file
 
 
 plink_path=$1
