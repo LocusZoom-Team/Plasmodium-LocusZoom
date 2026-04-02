95a029ad81cc03a33fe1ab8b06e9e35469f3d29b
kbokhari03 <kbokhari@luc.edu>
2026-04-02 16:20:56 -0500

    Added more comments

--- genelist/GetGeneList.py
+++ genelist/GetGeneList.py
@@ -13,23 +13,23 @@ with open("Pfalciparum3D7.gff") as f: #Open file
         if section[2] not in geneTypes: #Filter out anything that's not a gene
             continue #skip ahead to the next line
 
-        chromosome = section[0]
-        startPosition = int(section[3])
-        endPosition = int(section[4])
-        attributes = section[8]
+        chromosome = section[0] #Save chromosome 
+        startPosition = int(section[3]) #Save start position, and convert from int to string
+        endPosition = int(section[4]) #Save end position, and convert from int to string
+        attributes = section[8] #Save attributes, this will have our gene name (if present)
 
-        chromosomeMatch = re.search(r'Pf3D7_\d+_v3', chromosome)
+        chromosomeMatch = re.search(r'Pf3D7_\d+_v3', chromosome) #Use re package to filter our apicoplast and mitochondrial chromosomes out
 
-        if not chromosomeMatch:
-            continue
+        if not chromosomeMatch: #If apicoplast or mitochrondrial, skip
+            continue #continue to next line
 
         
 
-        attributesDict = {}
+        attributesDict = {} #Create attribute dictionary to store attributes for gene name
 
-        for attribute in attributes.split(";"):
-            key, value = attribute.split("=",1)
-            attributesDict[key.strip()] = value.strip()
+        for attribute in attributes.split(";"): #Split the attribute based on semicolon, that's how attribute is split
+            key, value = attribute.split("=",1) #Then split it again based on equal sign
+            attributesDict[key.strip()] = value.strip() #add name for key in dictionary, 
         
         if attributesDict.get("Name"):
             gene_id = attributesDict.get("Name")
