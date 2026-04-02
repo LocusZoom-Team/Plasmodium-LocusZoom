22d5f7e7621453f70810a7273127a09b8f2ba48d
gwegman7630 <gwegman@luc.edu>
2026-04-02 10:45:57 -0500

    Update test.R

--- Example/test.R
+++ Example/test.R
@@ -13,35 +13,35 @@ secondary = c("rs2540781", "rs8053279")
 #############################################we will need to change this so that user will specify different SNP names that isnt RS, also change gene list
 # create a LocusZoom-like plot
 source("functions/locus_zoom.R")
-locus.zoom(data = Example.assoc.linear, snp = "rs1008400", ld.file = Example.ld, offset = 200000, genes.data = UCSC_GRCh37_Genes_UniqueList.txt, noncoding = FALSE, plot.title = "Association of FTO with BMI in Europeans", nominal = 6, significant = 7.3, file.name = "Example.jpg", secondary.snp = NA, population = "EUR", sig.type = "P")
+locus.zoom(data = example.assoc.log, snp = "rs1008400", ld.file = Example.ld, offset = 200000, genes.data = UCSC_GRCh37_Genes_UniqueList.txt, noncoding = FALSE, plot.title = "Association of FTO with BMI in Europeans", nominal = 6, significant = 7.3, file.name = "Example.jpg", secondary.snp = NA, population = "EUR", sig.type = "P")
 
 # Testing with one secondary variant
 source("functions/locus_zoom.R")
-locus.zoom(data = Example.assoc.linear, snp = "rs1008400", ld.file = Example.ld, offset = 200000, genes.data = UCSC_GRCh37_Genes_UniqueList.txt, noncoding = FALSE, plot.title = "Association of FTO with BMI in Europeans", nominal = 6, significant = 7.3, file.name = "Example.jpg", secondary.snp = single, population = "EUR", sig.type = "P")
+locus.zoom(data = example.assoc.log, snp = "rs1008400", ld.file = Example.ld, offset = 200000, genes.data = UCSC_GRCh37_Genes_UniqueList.txt, noncoding = FALSE, plot.title = "Association of FTO with BMI in Europeans", nominal = 6, significant = 7.3, file.name = "Example.jpg", secondary.snp = single, population = "EUR", sig.type = "P")
 
 # Testing with two secondary variant
 source("functions/locus_zoom.R")
-locus.zoom(data = Example.assoc.linear, snp = "rs1008400", ld.file = Example.ld, offset = 200000, genes.data = UCSC_GRCh37_Genes_UniqueList.txt, noncoding = FALSE, plot.title = "Association of FTO with BMI in Europeans", nominal = 6, significant = 7.3, file.name = "Example.jpg", secondary.snp = secondary, population = "EUR", sig.type = "P")
+locus.zoom(data = example.assoc.log, snp = "rs1008400", ld.file = Example.ld, offset = 200000, genes.data = UCSC_GRCh37_Genes_UniqueList.txt, noncoding = FALSE, plot.title = "Association of FTO with BMI in Europeans", nominal = 6, significant = 7.3, file.name = "Example.jpg", secondary.snp = secondary, population = "EUR", sig.type = "P")
 
-# Testing with a larger data set
+# Testing with a larger data set #####DELETE
 Example.assoc.linear <- read.delim("eur_chr4.txt", stringsAsFactors = FALSE, header = TRUE)
 Example.ld <- read.table("rs145179124.ld", stringsAsFactors = FALSE, header = TRUE)
 
 Example.assoc.linear$P = Example.assoc.linear$P + .Machine$double.xmin
 
 source("functions/locus_zoom.R")
-locus.zoom(data = Example.assoc.linear, snp = "rs145179124", ld.file = Example.ld, offset = 500000, genes.data = UCSC_GRCh37_Genes_UniqueList.txt, noncoding = FALSE, plot.title = "EUR gout", nominal = 6, significant = 7.3, file.name = "rs145179124.jpg", secondary.snp = NA, population = "EUR", sig.type = "P")
+locus.zoom(data = example.assoc.log, snp = "rs145179124", ld.file = Example.ld, offset = 500000, genes.data = UCSC_GRCh37_Genes_UniqueList.txt, noncoding = FALSE, plot.title = "EUR gout", nominal = 6, significant = 7.3, file.name = "rs145179124.jpg", secondary.snp = NA, population = "EUR", sig.type = "P")
 
 # Try locating other loci as secondary SNP:
 loci = read.delim('EUR_meta_full1_clean_rsid_0.01.clumped.clean', stringsAsFactors = F, header = T) %>% select(1:5)
 
-Example.assoc.linear$P[which(Example.assoc.linear$SNP != 'rs145179124')] = Example.assoc.linear$P[which(Example.assoc.linear$SNP != 'rs145179124')] + .Machine$double.xmin
+example.assoc.log$P[which(example.assoc.log$SNP != 'rs145179124')] = example.assoc.log$P[which(example.assoc.log$SNP != 'rs145179124')] + .Machine$double.xmin
 secondary = loci$SNP[which(loci$SNP != 'rs145179124')]
 
 source("functions/locus_zoom.R")
-locus.zoom(data = Example.assoc.linear, snp = "rs145179124", ld.file = Example.ld, offset = 500000, genes.data = UCSC_GRCh37_Genes_UniqueList.txt, noncoding = FALSE, plot.title = "EUR gout", nominal = 6, significant = 7.3, file.name = "rs145179124.jpg", secondary.snp = secondary, population = "EUR", sig.type = "P", secondary.label = T)
+locus.zoom(data = example.assoc.log, snp = "rs145179124", ld.file = Example.ld, offset = 500000, genes.data = UCSC_GRCh37_Genes_UniqueList.txt, noncoding = FALSE, plot.title = "EUR gout", nominal = 6, significant = 7.3, file.name = "rs145179124.jpg", secondary.snp = secondary, population = "EUR", sig.type = "P", secondary.label = T)
 
-# Try with SLC2A9:
+# Try with SLC2A9: ######DELETE
 Example.assoc.linear <- read.delim("eur_chr4.txt", stringsAsFactors = FALSE, header = TRUE)
 Example.ld <- read.table("rs76242518.ld", stringsAsFactors = FALSE, header = TRUE)
 
